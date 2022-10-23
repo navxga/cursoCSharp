00de7445834fd894ec039c03f073f5abadaff8d8
lyannavegadev@gmail.com <lyannavegadev@gmail.com>
2022-10-23 11:35:58 -0300

    Estudando... cheguei na section 5

---
+++ section05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace section05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args[0] == "1")
+            {
+                if (args[1] == "1") Exercises01.ex01.Execute();
+            }
+        }
+    }
+}
